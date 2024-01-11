f57278a0b9dbdc05c2c2ba2c90f2582bba1eb3a8
Valentyna1402 <vriabchun@gmail.com>
2024-01-11 17:06:04 +0100

    finish

--- contacts.js
+++ contacts.js
@@ -1,24 +1,62 @@
-// const fs = require('fs').promises;
-// const path = require('path');
+import { nanoid } from "nanoid";
 
-// const contactsPath = path.basename('./db/contacts.json');
-// const contacts = require(contactsPath);
+import { promises as fs } from "fs";
+import { join } from "path";
 
-// export async function listContacts() {
-//     const data = await fs.readFile(contactsPath);
-//     console.log(data)
-//     return data;
-//     // ...твій код. Повертає масив контактів.
-//   }
-  
-//   async function getContactById(contactId) {
-//     // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
-//   }
-  
-//   async function removeContact(contactId) {
-//     // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
-//   }
-  
-//   async function addContact(name, email, phone) {
-//     // ...твій код. Повертає об'єкт доданого контакту (з id).
-//   }
+const contactsPath = join("db", "contacts.json");
+
+export async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find(({ id }) => id === contactId);
+    return contact || null;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function updateContacts(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+}
+
+export async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex((contact) => contact.id === contactId);
+    if (idx === -1) {
+      return null;
+    }
+    const deletedContact = contacts.splice(idx, 1);
+    await updateContacts(contacts);
+    return deletedContact;
+  } catch (error) {
+    console.error(err);
+  }
+}
+
+export async function addContact(name, email, phone) {
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  try {
+    const contacts = await listContacts();
+    contacts.push(newContact);
+    updateContacts(contacts);
+    return newContact;
+  } catch (err) {
+    console.error(err);
+  }
+}
--- index.js
+++ index.js
@@ -1,90 +1,46 @@
-// import { program } from "commander";
-// program
-//   .option("-a, --action <type>", "choose action")
-//   .option("-i, --id <type>", "user id")
-//   .option("-n, --name <type>", "user name")
-//   .option("-e, --email <type>", "user email")
-//   .option("-p, --phone <type>", "user phone");
+import { program } from "commander";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "./contacts.js";
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-// program.parse();
+program.parse();
 
-// const options = program.opts();
+const options = program.opts();
 
-// // TODO: рефакторити
-// async function invokeAction({ action, id, name, email, phone }) {
-//   switch (action) {
-//     case "list":
-//       // ...
-//       break;
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      console.table(allContacts);
+      break;
 
-//     case "get":
-//       // ... id
-//       break;
+    case "get":
+      const contact = await getContactById(id);
+      console.log(contact);
+      break;
 
-//     case "add":
-//       // ... name email phone
-//       break;
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
+      break;
 
-//     case "remove":
-//       // ... id
-//       break;
+    case "remove":
+      const removedContact = await removeContact(id);
+      console.log(removedContact);
+      break;
 
-//     default:
-//       console.warn("\x1B[31m Unknown action type!");
-//   }
-// }
-
-// invokeAction(options);
-
-
-import { nanoid } from 'nanoid';
-
-import { promises as fs } from 'fs';
-import { join } from 'path';
-
-
-const contactsPath = join('db', 'contacts.json');
-
-
- async function listContacts() {
-    const data = (await fs.readFile(contactsPath)).toString();
-    return(data);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
   }
-  
-  async function getContactById(contactId) {
-    const data = (await fs.readFile(contactsPath)).toString();
-    const parsedData = JSON.parse(data);
-    return parsedData.map(item => {
-        if (item.id === contactId) {
-           return item; 
-        } else {
-            return null;
-        }})
-  }
-  
-  async function removeContact(contactId) {
-    const data = (await fs.readFile(contactsPath)).toString();
-    const parsedData = JSON.parse(data);
-    return parsedData.map(item => {
-        if (item.id === contactId) {
-           return item; 
-        } else {
-            return null;
-        }})
-  }
-  
-  async function addContact(name, email, phone) {
-    const newContact = {id: nanoid(), name, email, phone};
-    const newContactString = JSON.stringify(newContact);
-    const data = await fs.writeFile(contactsPath, newContactString, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!')
-    })
-    console.log(data)
-    return data;
-    //console.log(data)
-   // return fs.writeFile(contactsPath, newContact)
-    // ...твій код. Повертає об'єкт доданого контакту (з id).
-  }
+}
 
-  addContact('name', 'email', 'phone')
+invokeAction(options);
